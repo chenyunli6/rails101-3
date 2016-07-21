38aabf33985a7db04151236e3e8b16eb7b19f79a
chenyl <18301662790@163.com>
2016-07-21 22:08:02 +0800

    implement my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!,:only=>[:new,:create]
+  before_action :find_post_and_check_permission,:only=>[:edit,:update,:destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -19,7 +20,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice:"Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path,alert:"Post Deleted "
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user!=@group.user
+      redirect_to root_paths,alert:"You have no permission"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
